ead20f8a5299f36765553a3e5474af89e17d4782
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-08 17:15:44 +0300

    Add project files.

---
+++ fake-data-api/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fake_data_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        
+    }
+}
