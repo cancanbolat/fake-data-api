78e79bc9706d3cb41d40068b050f06735dbba38c
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-09 13:20:21 +0300

    Add User model class

---
+++ fake-data-api/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fake_data_api.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Street { get; set; }
+        public string Suite { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
