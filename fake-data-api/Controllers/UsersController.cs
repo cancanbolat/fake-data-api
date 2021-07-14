3b4d9620141fadbc0741ca96b5d1be8778e43e48
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-07-14 12:11:07 +0300

    Update UserController

--- fake-data-api/Controllers/UsersController.cs
+++ fake-data-api/Controllers/UsersController.cs
@@ -23,10 +23,10 @@ namespace fake_data_api.Controllers
         }
         
         //BOGUS
-        [HttpGet("{count}")]
-        public ActionResult Get(int count)
+        [HttpGet("{language}/{count}")]
+        public ActionResult Get(int count, string language = "en")
         {
-            var fakeUser = new Faker<User>("tr")
+            var fakeUser = new Faker<User>(language)
                 .RuleFor(u => u.Id, f => f.Random.Guid())
                 .RuleFor(u => u.FirstName, f => f.Person.FirstName)
                 .RuleFor(u => u.LastName, f => f.Person.LastName)
@@ -45,3 +45,56 @@ namespace fake_data_api.Controllers
         }
     }
 }
+
+/*
+ af_ZA	Afrikaans		
+fr_CH	French (Switzerland)
+ar	Arabic		
+ge	Georgian
+az	Azerbaijani		
+hr	Hrvatski
+cz	Czech		
+id_ID	Indonesia
+de	German		
+it	Italian
+de_AT	German (Austria)		
+ja	Japanese
+de_CH	German (Switzerland)		
+ko	Korean
+el	Greek		
+lv	Latvian
+en	English		
+nb_NO	Norwegian
+en_AU	English (Australia)		
+ne	Nepalese
+en_AU_ocker	English (Australia Ocker)		
+nl	Dutch
+en_BORK	English (Bork)		
+nl_BE	Dutch (Belgium)
+en_CA	English (Canada)		
+pl	Polish
+en_GB	English (Great Britain)		
+pt_BR	Portuguese (Brazil)
+en_IE	English (Ireland)		
+pt_PT	Portuguese (Portugal)
+en_IND	English (India)		
+ro	Romanian
+en_NG	Nigeria (English)		
+ru	Russian
+en_US	English (United States)		
+sk	Slovakian
+en_ZA	English (South Africa)		
+sv	Swedish
+es	Spanish		
+tr	Turkish
+es_MX	Spanish (Mexico)		
+uk	Ukrainian
+fa	Farsi		
+vi	Vietnamese
+fi	Finnish		
+zh_CN	Chinese
+fr	French		
+zh_TW	Chinese (Taiwan)
+fr_CA	French (Canada)		
+zu_ZA	Zulu (South Africa)
+ */
